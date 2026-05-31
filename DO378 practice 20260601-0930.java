a76a0782108b733b3c58aac9610840d5d8282327
Jamie Burns <jamie-burns0@github.com>
2026-06-01 09:49:01 +1000

    save point

---
+++ DO378 practice 20260601-0930.java
@@ -0,0 +1,103 @@
+Health: smallrye-health
+Fault tolerance: smallrye-fault-tolerance
+Metrics: quarkus-microprofile, quarkus-microprofile-registry-prometheus
+Opentelemetry: quarkus-opentelemetry
+
+## Health
+
+@Ready
+@ApplicationScoped
+public class ReadyCheck extends HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("ready-check");
+    }
+}
+
+@Startup
+@ApplicationScoped
+public class StartupCheck extends HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("startup-check")
+            .up()
+            .build();
+    }
+}
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck extends HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.builder()
+            .name("liveness-check")
+            .up()
+            .withData("key", "value")
+            .build();
+    }
+}
+
+## Fault tolerance
+
+public class SomeResource {
+    @GET
+    @Retry(maxRetries=4, retryOn=SomeException.class)
+    @Timeout(3000)
+    @Fallback(fallbackMethod="someFallback")
+    public List<Thing> list() {
+        try {
+            return ...
+        } catch(TimeoutException e) {
+            // do something
+        }
+    }
+
+    public List<Thing> someFallback() {
+        return ... // canned list of Thing
+    }
+
+    @GET
+    @CircuitBreaker(...Threshold = 4)
+    ...
+}
+
+## Metrics
+
+public class SomeResource {
+    @Inject
+    MeterRegistry registry;
+
+    @GET
+    public List<Thing> countedList() {
+        registry.counter("countedList.call.counter").increment();
+        ...        
+    }
+
+    @GET
+    public List<Thing> timedList() {
+        registry.timer("timedList.timer").???()
+            .wrap(
+                (Supplier<List<Thing>>) () -> {
+                    return ... // get our list of Thing
+                }
+            )
+            .get()
+    }
+
+    // Guage: TODO
+}
+
+## Open telemetry
+
+public class SomeResource {
+    @GET
+    @WithSpan("my-span-name")
+    ...
+
+    @GET
+    @WithSpanAttributes
+    public List<Thing> listThingsOverAge(@SpanAttribute("thingAge") String thingAge) {
+        ...
+    }
+}
